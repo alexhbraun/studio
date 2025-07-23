4ef9f7a071096ee853e507bcc6419ece3e1f1dcf
Alexander Braun <alexanderhelmutbraun@gmail.com>
2025-07-23 20:45:02 +0000

    when i try to register i get this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,9 +2,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { config } from 'dotenv';
-
-config({ path: '.env' });
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
