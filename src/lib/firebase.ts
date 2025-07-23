391da06f73fb162b845663cbfee753281b02d4ef
Alexander Braun <alexanderhelmutbraun@gmail.com>
2025-07-23 21:31:39 +0000

    please confirm if it was restarted [studio-8022629385:~/studio{master}$

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBk5oIWJ_ujhPF71He6LgncqAxdmM29cqQ",
+  authDomain: "stridesculpt.firebaseapp.com",
+  projectId: "stridesculpt",
+  storageBucket: "stridesculpt.firebasestorage.app",
+  messagingSenderId: "596337424461",
+  appId: "1:596337424461:web:203d266d5d4dce8d41c404",
 };
 
 // Initialize Firebase
